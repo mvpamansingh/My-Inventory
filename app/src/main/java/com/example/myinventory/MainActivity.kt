be79a350d129c31cc85165f99c0163b69c1060a9
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2023-11-05 22:22:18 +0530

    del

--- app/src/main/java/com/example/myinventory/MainActivity.kt
+++ app/src/main/java/com/example/myinventory/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.myinventory
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myinventory.ui.theme.MyInventoryTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,27 +43,112 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
-                    
-                        ScreenSetup()
+
+                    val owner = LocalViewModelStoreOwner.current
+                    owner?.let {
+                        val viewModel: MainViewModel = viewModel(
+                            it,
+                            "MainViewModel",
+                            MainViewModelFactory(
+                                LocalContext.current.applicationContext
+                                        as Application)
+                        )
+                        ScreenSetup(viewModel)
+                    }
+                       // ScreenSetup()
                 }
             }
         }
     }
 }
+class MainViewModelFactory(val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(application) as T
+    }
+}
+
+
+
+
+
+
 
 
 
 @Composable
-fun ScreenSetup()
+fun TitleRow(head1: String, head2: String, head3: String) {
+    Row(
+        modifier = Modifier
+           // .background(MaterialTheme.colors.primary)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(head1, color = Color.White,
+            modifier = Modifier
+                .weight(0.1f))
+        Text(head2, color = Color.White,
+            modifier = Modifier
+                .weight(0.2f))
+        Text(head3, color = Color.White,
+            modifier = Modifier.weight(0.2f))
+    }
+}
+
+@Composable
+fun ProductRow(id: Int, name: String, quantity: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(id.toString(), modifier = Modifier
+            .weight(0.1f))
+        Text(name, modifier = Modifier.weight(0.2f))
+        Text(quantity.toString(), modifier = Modifier.weight(0.2f))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit,
+    keyboardType: KeyboardType
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { onTextChange(it) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        singleLine = true,
+        label = { Text(title)},
+        modifier = Modifier.padding(10.dp),
+        textStyle = TextStyle(fontWeight = FontWeight.Bold,
+            fontSize = 30.sp)
+    )
+}
+@Composable
+fun ScreenSetup(viewModel:MainViewModel)
 {
-    MainScreen()
+    //val allProducts by viewModel.allProducts.observeAsState(listOf<Product>())
+    val allProducts by viewModel.allProducts.observeAsState(listOf())
+    val searchResults by viewModel.searchResults.observeAsState(listOf())
+    MainScreen(
+        allProducts = allProducts,
+        searchResults = searchResults,
+        viewModel = viewModel
+    )
     
 }
 
 
 @Composable
-fun MainScreen()
+fun MainScreen(allProducts: List<Product>,
+               searchResults: List<Product>,
+               viewModel: MainViewModel)
 {
     Text(text = "Working !")
 }
